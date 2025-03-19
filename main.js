c647f1c048e08be759bd77a555825c7e52e6a5cb
Laci5555 <147511876+Laci5555@users.noreply.github.com>
2025-03-19 10:57:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const url = "https://raw.githubusercontent.com/Laci5555/portfolio_json/refs/heads/main/data_json"
+
+getData(url, renderData)
+
+function renderData(data){
+    console.log(data);
+    
+}
