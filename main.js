e8661c6a5f592c5bc0098d4c7a193ac4656f68e5
Laci5555 <147511876+Laci5555@users.noreply.github.com>
2025-03-25 19:03:06 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const url = "https://raw.githubusercontent.com/Laci5555/portfolio_json/refs/heads/main/data_json"
-
-getData(url, renderData)
-
-function renderData(data){
-    console.log(data);
-    
-}
